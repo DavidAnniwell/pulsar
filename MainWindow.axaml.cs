c838c7bd389b5c16bd98ae033bcc63437fb6f38e
DavidAnniwell <davidanniwell@gmail.com>
2022-06-11 17:46:21 +0100

    Initial commit

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace PulsarUIMock
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
